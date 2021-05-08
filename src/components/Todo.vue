138a8e5eafaa0dc6d3202809a982a2c7102ccdf9
Ayoazeez26 <ayoazeez26@gmail.com>
2021-05-08 09:28:55 +0100

    making progress

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-1/2 mx-auto">
+  <div class="w-1/2 mx-auto flex flex-col items-center">
     <h1 class="text-4xl text-gray-700 font-bold mb-12">#todo</h1>
     <div
-      class="header relative text-sm flex justify-between font-semibold items-center text-gray-700"
+      class="header relative text-sm flex justify-between font-semibold items-center text-gray-700 w-full"
     >
       <h4
         v-for="(tab, index) in tabs"
@@ -14,26 +14,69 @@
         {{ tab }}
       </h4>
     </div>
-    <div class="all" v-show="selectedTab === 'All'">
-      <form @submit.prevent="addTodo">
+    <div
+      class="all mt-6 w-full"
+      v-show="selectedTab === 'All' || selectedTab === 'Active'"
+    >
+      <form class="flex justify-between items-center" @submit.prevent="addTodo">
         <input
           type="text"
           placeholder="add details"
           v-model="newTodo"
-          class="border rounded-lg p-3"
+          class="border focus:outline-none rounded-lg p-3 w-3/4"
         />
+        <button
+          class="bg-blue-500 focus:outline-none font-semibold text-sm text-white py-3 px-6 rounded-lg"
+          type="submit"
+        >
+          Add
+        </button>
       </form>
+      <ul class="mt-3">
+        <li
+          class="flex items-center"
+          v-for="(todo, index) in getTodos"
+          :key="index"
+        >
+          <input
+            type="checkbox"
+            v-model="checkedTasks"
+            class="checkbox mr-2 w-5 h-5"
+          />
+          <label class="text-black font-medium text-lg" for="checkbox">
+            {{ todo.task }}
+          </label>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
       tabs: ["All", "Active", "Completed"],
       selectedTab: "All",
+      newTodo: "",
+      checkedTasks: [],
     };
+  },
+  computed: {
+    ...mapGetters(["getTodos"]),
+  },
+  methods: {
+    addTodo() {
+      if (this.todo != "") {
+        const newTodo = {
+          task: this.newTodo,
+          active: true,
+        };
+        this.newTodo = "";
+        this.$store.dispatch("addTodo", newTodo);
+      }
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    allTodos: [],
+  },
+  getters: {
+    getTodos: (state) => {
+      return state.allTodos;
+    },
+  },
+  mutations: {
+    addTodo: (state, payload) => {
+      state.allTodos.push(payload);
+    },
+  },
+  actions: {
+    addTodo: (context, payload) => {
+      context.commit("addTodo", payload);
+    },
+  },
   modules: {},
 });
